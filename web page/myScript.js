71c8640e2f6ec8f8f9896a4a8198bafa5393e367
Yogeshvarman <127743066+Yogeshvarman@users.noreply.github.com>
2023-03-17 16:19:13 +0530

    Add files via upload

---
+++ web page/myScript.js
@@ -0,0 +1,6 @@
+const cars = ["saab","Volvo","Lamborgini"];
+const products = ["laptop","smart watch","mobile","electronic gadgets"];
+
+    document.getElementById("variableNote").innerHTML = cars.concat(products);
+    
+
